04badfa019276886e88651a70ee4acc98b48be22
Dániel Buga <bugadani@gmail.com>
2017-07-31 13:06:05 +0200

    Re-add method to retrieve all parameters

--- src/Variations/AlternativeComponentVariation.php
+++ src/Variations/AlternativeComponentVariation.php
@@ -8,11 +8,13 @@ use AltiumParser\Component\Parameter;
 class AlternativeComponentVariation extends ComponentVariation
 {
     private $parameters = [];
+    private $parameterMap = [];
 
     public function __construct(array $variation, array $parameters)
     {
         parent::__construct($variation);
         foreach ($parameters as $parameter => $value) {
+            $this->parameterMap[$parameter] = $value;
             $this->parameters[] = [
                 'RECORD'      => 41,
                 'NAME'        => $parameter,
@@ -34,5 +36,13 @@ class AlternativeComponentVariation extends ComponentVariation
             ],
             'parameters' => $this->parameters
         ]);
+    }
+
+    /**
+     * @return array
+     */
+    public function getParameters()
+    {
+        return $this->parameterMap;
     }
 }
